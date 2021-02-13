6cf2744fd22945473c211910f92c36f506776bae
Bogdanstt <53273734+Bogdanstt@users.noreply.github.com>
2021-02-13 18:51:08 +0200

    create react app, add palette comp

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* navbar */}
+        <div className="Palette-colors"></div>
+        {/* footer */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
