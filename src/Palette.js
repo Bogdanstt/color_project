04ac522b303cfaf3a8d474bbf5036472d4d3ea83
Bogdanstt <53273734+Bogdanstt@users.noreply.github.com>
2021-02-18 20:07:31 +0200

    Style slider

--- src/Palette.js
+++ src/Palette.js
@@ -20,15 +20,40 @@ class Palette extends Component {
     const colorBoxes = colors[level].map((color) => (
       <ColorBox background={color.hex} name={color.name} />
     ));
+    const railStyle = { height: "10px" };
+    const handleStyle = [
+      {
+        backgroundColor: "green",
+        outline: "none",
+        border: "2px solid green",
+        boxShadow: "none",
+        width: "15px",
+        height: "15px",
+        marginLeft: "-7px",
+        marginTop: "-2.75px",
+      },
+    ];
+    const trackStyle = [
+      {
+        backgroundColor: "transparent",
+      },
+    ];
+
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+            railStyle={railStyle}
+            handleStyle={handleStyle}
+            trackStyle={trackStyle}
+          />
+        </div>
+
         {/* navbar */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* footer */}
