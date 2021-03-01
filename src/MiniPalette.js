b78bb22a2f3c8cc85a2614ad8837a506f2b9055e
Bogdanstt <53273734+Bogdanstt@users.noreply.github.com>
2021-03-01 19:07:40 +0200

    Add Link to Palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -51,7 +51,7 @@ function MiniPalette(props) {
     />
   ));
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={props.handleClick}>
       <div className={classes.colors}>{minicolorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName}
