fa3b2623d740fd349ebf94c0ecfd85d5eac02a8c
Bogdanstt <53273734+Bogdanstt@users.noreply.github.com>
2021-05-17 19:10:28 +0300

    Prevent duplicate random colors

--- src/App.js
+++ src/App.js
@@ -104,6 +104,17 @@ class App extends Component {
                     </Page>
                   )}
                 />
+                <Route
+                  render={(routeProps) => (
+                    <Page>
+                      <PaletteList
+                        palettes={palettes}
+                        deletePalette={this.deletePalette}
+                        {...routeProps}
+                      />
+                    </Page>
+                  )}
+                />
               </Switch>
             </CSSTransition>
           </TransitionGroup>
